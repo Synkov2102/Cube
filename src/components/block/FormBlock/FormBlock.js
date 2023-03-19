5fa1e3cd96e19d853ed885e9ca24e80cb269e47b
Eugeny <synkoveugeny@gmail.com>
2023-03-19 22:29:41 +0300

    small fixes

--- src/components/block/FormBlock/FormBlock.js
+++ src/components/block/FormBlock/FormBlock.js
@@ -5,18 +5,20 @@ import Form from '@/components/UI/Form/Form';
 
 export default function FormBlock() {
   return (
-    <Container>
-      <section className={styles.banner}>
-        <div className={styles.content}>
-          <p className={styles.text}>НУЖНА КОНСУЛЬТАЦИЯ?</p>
-          <h2 className={styles.title}>
-            Узнайте больше и ознакомьтесь с нашими ценами.
-          </h2>
+    <section className={styles.background}>
+      <Container>
+        <div className={styles.banner}>
+          <div className={styles.content}>
+            <p className={styles.text}>ЛУЧШИЕ УСЛОВИЯ</p>
+            <h2 className={styles.title}>
+              Оставьте заявку сейчас и получите специальное предложение.
+            </h2>
+          </div>
+          <div className={styles.form}>
+            <Form />
+          </div>
         </div>
-        <div className={styles.form}>
-          <Form />
-        </div>
-      </section>
-    </Container>
+      </Container>
+    </section>
   );
 }
